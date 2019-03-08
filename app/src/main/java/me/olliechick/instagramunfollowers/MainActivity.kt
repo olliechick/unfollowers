0219ea5d31f5bed8e2a9f040fb8f87cb9263874e
Ollie Chick <och26@uclive.ac.nz>
2019-03-09 00:28:34 +1300

    Create an intent for clicking the button.
    - Create an InstagramApiInterface file for storing stuff related to Instagram's API.
    - Make button go to Instagram's auth website, and link back to olliechick.me (for now)

--- app/src/main/java/me/olliechick/instagramunfollowers/MainActivity.kt
+++ app/src/main/java/me/olliechick/instagramunfollowers/MainActivity.kt
@@ -1,9 +1,11 @@
 package me.olliechick.instagramunfollowers
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import me.olliechick.instagramunfollowers.AUTH_URI as AUTH_URI
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +17,7 @@ class MainActivity : Activity() {
     fun logIn(view: View) {
         // Do something in response to button
         Toast.makeText(this, "You clicked log in!", Toast.LENGTH_LONG).show()
+        val intent = Intent(Intent.ACTION_VIEW, AUTH_URI)
+        startActivity(intent)
     }
 }
