4dd9c349d2a1cc633215327723a42111af301ccb
Ollie Chick <och26@uclive.ac.nz>
2019-03-09 00:29:04 +1300

    Create account list activity.

---
+++ app/src/main/java/me/olliechick/instagramunfollowers/AccountListActivity.kt
@@ -0,0 +1,12 @@
+package me.olliechick.instagramunfollowers
+
+import android.app.Activity
+import android.os.Bundle
+
+class AccountListActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account_list)
+    }
+}
