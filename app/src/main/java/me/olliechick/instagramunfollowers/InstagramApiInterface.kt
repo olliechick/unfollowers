69d58c1ac43db9a588bf251691e5f528ac0bfe54
Ollie Chick <och26@uclive.ac.nz>
2019-03-11 01:04:59 +1300

    Remove main activity and instagram api interface. create instagram authorise activity, and add a bunch of icons (blue background, watermelon foreground). Add Anko and Instagram4Android to gradle build file. Set account list activity as main activity, and browsable activity. add a bunch of strings to the files (and for french).

--- app/src/main/java/me/olliechick/instagramunfollowers/InstagramApiInterface.kt
+++
@@ -1,8 +0,0 @@
-package me.olliechick.instagramunfollowers
-
-import android.net.Uri
-
-val CLIENT_ID = "61371bd49fc440efacbeac38611acb09"
-val REDIRECT_URI = "http://olliechick.me"
-val AUTH_URI = Uri.parse("https://api.instagram.com/oauth/authorize/" +
-        "?client_id=$CLIENT_ID&redirect_uri=$REDIRECT_URI&response_type=token")
