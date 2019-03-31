5956eeeb0535136f5c865e9e1b6dced4219a99ca
Ollie Chick <och26@uclive.ac.nz>
2019-03-31 16:59:34 +1300

    add unfollower list (that is for the moment a copy of the list of accounts) add refresh FAB

--- app/src/main/java/me/olliechick/instagramunfollowers/UnfollowersListActivity.kt
+++ app/src/main/java/me/olliechick/instagramunfollowers/UnfollowersListActivity.kt
@@ -1,23 +1,69 @@
 package me.olliechick.instagramunfollowers
 
-import android.app.Activity
+import android.arch.persistence.room.Room
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.TextView
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_account_list.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.toast
 
 class UnfollowersListActivity : AppCompatActivity() {
+    private lateinit var unfollowerList: RecyclerView
+    private lateinit var db: AppDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_unfollowers_list)
 
-        val textView = findViewById<TextView>(R.id.textView)
-
-        val username = getIntent().getStringExtra("username")
+        val username = intent.getStringExtra("username")
 
         val actionBar = supportActionBar
-        actionBar?.title = "Unfollowers: $username"
+        actionBar?.title = "${getString(R.string.app_name)}: $username"
 
-        textView.text = "hello!"
+        fab.setOnClickListener { refresh() }
+
+        populateList()
+    }
+
+    var unfollowers: ArrayList<Account> = arrayListOf()
+        set(value) {
+            field = value
+            accountList.adapter = AccountAdapter(this, field) {
+                val intent = Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://www.instagram.com/${it.username}/")
+                )
+                startActivity(intent)
+            }
+        }
+
+    private fun getUnfollowerDao(): AccountDao{
+        db = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java, "db"
+        ).build()
+        return db.accountDao()
+    }
+
+    private fun populateList() {
+        unfollowerList = findViewById(R.id.accountList)
+        val layoutManager = LinearLayoutManager(this)
+        accountList.layoutManager = layoutManager
+
+        doAsync {
+            unfollowers = ArrayList(getUnfollowerDao().getAll())
+        }
+
+        val decoration = DividerItemDecoration(this, layoutManager.orientation)
+        accountList.addItemDecoration(decoration)
+    }
+
+    private fun refresh() {
+        toast("Refreshing not yet implemented.")
     }
 }
