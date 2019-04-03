f2e09306aad9f24b538609016de508341a471129
Ollie <och26@uclive.ac.nz>
2019-04-03 18:17:48 +1300

    remove code that keeps track of selected account as it isn't necessary

--- app/src/main/java/me/olliechick/instagramunfollowers/AccountAdapter.kt
+++ app/src/main/java/me/olliechick/instagramunfollowers/AccountAdapter.kt
@@ -15,21 +15,13 @@ class AccountAdapter(
     val clickListener: (Account) -> Unit
 ) : RecyclerView.Adapter<AccountViewHolder>() {
 
-    private var selectedIndex = RecyclerView.NO_POSITION
-
     override fun getItemCount(): Int = accounts.size
 
     override fun onCreateViewHolder(parent: ViewGroup, p1: Int): AccountViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.account_item, parent, false)
         val holder = AccountViewHolder(view)
 
-        holder.itemView.setOnClickListener {
-            val oldSelectedIndex = selectedIndex
-            selectedIndex = holder.adapterPosition
-            clickListener(accounts[holder.adapterPosition])
-            notifyItemChanged(oldSelectedIndex)
-            notifyItemChanged(selectedIndex)
-        }
+        holder.itemView.setOnClickListener { clickListener(accounts[holder.adapterPosition]) }
 
         return holder
     }
@@ -39,7 +31,7 @@ class AccountAdapter(
         val username = accounts[i].username
         val created = accounts[i].created
 
-        var displayText  = ""
+        var displayText = ""
         if (name.replace("\\s".toRegex(), "") == "") displayText = username
         else displayText = "$name ($username)"
         displayText += " (${created.format(DateTimeFormatter.ofPattern("d MMM"))})"
