48b2b2edf8ed1189626940e39e30970fc6a2587f
Ollie <och26@uclive.ac.nz>
2019-04-03 18:20:12 +1300

    create followeradapter (from copying accountadapter - this needs to be changed!!)

---
+++ app/src/main/java/me/olliechick/instagramunfollowers/FollowerAdapter.kt
@@ -0,0 +1,43 @@
+package me.olliechick.instagramunfollowers
+
+import android.content.Context
+import android.graphics.Color
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import java.time.format.DateTimeFormatter
+
+class FollowerAdapter(
+    private val context: Context,
+    val followers: ArrayList<Follower>,
+    val clickListener: (Follower) -> Unit
+) : RecyclerView.Adapter<AccountViewHolder>() {
+
+    private var selectedIndex = RecyclerView.NO_POSITION
+
+    override fun getItemCount(): Int = followers.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, p1: Int): AccountViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.account_item, parent, false)
+        val holder = AccountViewHolder(view)
+
+        holder.itemView.setOnClickListener { clickListener(followers[holder.adapterPosition]) }
+
+        return holder
+    }
+
+    override fun onBindViewHolder(holder: AccountViewHolder, i: Int) {
+        val name = followers[i].name
+        val username = followers[i].username
+        val created = followers[i].timestamp
+
+        var displayText  = ""
+        if (name.replace("\\s".toRegex(), "") == "") displayText = username
+        else displayText = "$name ($username)"
+        displayText += " (${created.format(DateTimeFormatter.ofPattern("d MMM"))})"
+
+        holder.accountName.text = displayText
+    }
+}
