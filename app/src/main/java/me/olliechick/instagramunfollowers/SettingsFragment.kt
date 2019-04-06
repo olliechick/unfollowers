544af05971e8cad3740dea2d44bff3e837f28900
Ollie Chick <och26@uclive.ac.nz>
2019-04-06 22:50:48 +1300

    backend for 'send feedback' button

--- app/src/main/java/me/olliechick/instagramunfollowers/SettingsFragment.kt
+++ app/src/main/java/me/olliechick/instagramunfollowers/SettingsFragment.kt
@@ -1,10 +1,24 @@
 package me.olliechick.instagramunfollowers
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+        initSettings()
     }
+
+    private fun initSettings() {
+        findPreference("feedback").setOnPreferenceClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.data = Uri.parse("mailto:olliechick@gmail.com");
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Unfollowers app")
+            startActivity(intent)
+            true
+        }
+    }
+
 }
