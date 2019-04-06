9cb632cd60e6a53e765772220005cfc5be16880b
Ollie Chick <och26@uclive.ac.nz>
2019-04-07 02:45:00 +1300

    use string resources more

--- app/src/main/java/me/olliechick/instagramunfollowers/SettingsFragment.kt
+++ app/src/main/java/me/olliechick/instagramunfollowers/SettingsFragment.kt
@@ -1,6 +1,7 @@
 package me.olliechick.instagramunfollowers
 
 import android.content.Intent
+import android.content.res.Resources
 import android.net.Uri
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
@@ -14,8 +15,8 @@ class SettingsFragment : PreferenceFragmentCompat() {
     private fun initSettings() {
         findPreference("feedback").setOnPreferenceClickListener {
             val intent = Intent(Intent.ACTION_SENDTO)
-            intent.data = Uri.parse("mailto:olliechick@gmail.com");
-            intent.putExtra(Intent.EXTRA_SUBJECT, "Unfollowers app")
+            intent.data = Uri.parse("mailto:" + Resources.getSystem().getString(R.string.dev_email))
+            intent.putExtra(Intent.EXTRA_SUBJECT, Resources.getSystem().getString(R.string.app_name))
             startActivity(intent)
             true
         }
